8c8b31d5bf92649159015046d17749cf7e2748a6
Divya-K2517 <115424525+Divya-K2517@users.noreply.github.com>
2024-12-24 02:22:24 -0500

    added dino and cacti

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+import random 
+import time
 
 pygame.init()
 WIDTH, HEIGHT = 1000, 300
@@ -28,8 +30,46 @@ class Ground():
             self.rect1.left = self.rect2.right
         if self.rect2.right < 0:
             self.rect2.left = self.rect1.right
+
+class Cactus(pygame.sprite.Sprite):
+    def __init__(self, speed=5):
+        super().__init__()
+        self.image = pygame.image.load("T-rex game/cactus.png")
+        self.rect = self.image.get_rect()
+        self.rect.bottom = HEIGHT
+        self.rect.right = WIDTH
+        self.speed = speed
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+        
+    def update(self):
+        self.rect.left -= self.speed
+        if self.rect.right <= 0:
+            self.kill()
+
+class Dino():
+    def __init__(self):
+        self.image = pygame.image.load("T-rex game/dino.png")
+        self.image = pygame.transform.scale(self.image, (100,100))
+        self.rect = self.image.get_rect()
+        self.rect.left = 10
+        self.rect.bottom = int(HEIGHT*0.96) 
+    def draw(self):
+        screen.blit(self.image, self.rect)
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]: #press space bar to make dino jump
+            self.rect.bottom = int(HEIGHT*0.96) - 100
+        elif not keys[pygame.K_SPACE]:
+            self.rect.bottom = int(HEIGHT*0.96) 
+
+
 #objects
 ground = Ground()
+dino = Dino()
+cacti = pygame.sprite.Group()
+cactus = Cactus()
+cacti.add(cactus)
 
 #main loop
 while running:
@@ -43,6 +83,31 @@ while running:
     #game code
     ground.draw()
     ground.update()
+
+    if len(cacti) < 3:
+        if len(cacti) == 0:
+            cacti.add(Cactus())
+        else:
+            for c in cacti:
+                if c.rect.right < WIDTH*0.6 and random.randint(0,50) == 10:
+                    new_cactus = Cactus()
+                    cacti.add(new_cactus)
+                    if random.randint(0,40) == 10:
+                        new_cactus2 = new_cactus
+                        new_cactus2.rect.left = new_cactus.rect.right
+                        cacti.add(new_cactus2)
+            
+
+    cacti.draw(screen)
+    cacti.update()
+
+    dino.draw()
+    dino.update()
+
+
+
+
+    
 
     pygame.display.flip() #updates display
 
